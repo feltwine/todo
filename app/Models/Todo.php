91bd77da5327fecc36dffdb8c97a9e9ff5cb3056
feltwine <patik.vaclav@proton.me>
2025-04-02 12:00:49 +0200

    Init commit for todo app working with discord

---
+++ app/Models/Todo.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property boolean $completed
+ *
+ * @property string|Carbon $created_at
+ * @property string|Carbon $updated_at
+ * @property string|Carbon|null $deleted_at
+ *
+ * @mixin Builder
+ */
+class Todo extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'completed',
+    ];
+
+    protected $casts = [
+      'created_at' => 'datetime',
+      'updated_at' => 'datetime',
+    ];
+}
